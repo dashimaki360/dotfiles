feb64ea7e956c4360149ef79da2f94b424d4f354
dashimaki360 <17.punch@gmail.com>
2017-12-28 12:49:51 +0900

    add ros_kinetic_insatall

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -7,6 +7,10 @@ set -x
 sudo apt-get update
 sudo apt-get install -y git vim-gnome curl
 
+# git config setting
+git config --global user.email "17.punch@gmail.com"
+git config --global user.name "dashimaki360"
+
 #install neobundle
 #curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
 #sh ./install.sh
